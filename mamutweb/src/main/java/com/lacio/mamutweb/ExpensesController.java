908a6f6da6f7bc6ca1c1799cb59ebe38b7f18b26
Piotr Zorychta <petersohn100@gmail.com>
2022-04-20 20:37:11 +0200

    add expenses list

--- mamutweb/src/main/java/com/lacio/mamutweb/ExpensesController.java
+++ mamutweb/src/main/java/com/lacio/mamutweb/ExpensesController.java
@@ -2,6 +2,7 @@ package com.lacio.mamutweb;
 
 import com.lacio.mamutweb.entities.SettlementRow;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,14 +17,23 @@ public class ExpensesController {
     }
 
     @GetMapping("/expenses")
-    public ExpensesOutput expensesOutput(){
+    public ExpensesOutput expensesOutput(@RequestParam(required = false) String category){
+
 
         ArrayList<Expense> expenses = new ArrayList<>();
         List<SettlementRow> rows = repository.findAll();
-        for (SettlementRow row:rows) {
-            expenses.add(new Expense(row.getId(),row.getKategoria(),row.getKoszt(),Character.toString(row.getKto())));
+        if (category==null) {
+            for (SettlementRow row : rows) {
+                expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
+            }
+        } else {
+        for (SettlementRow row : rows) {
+            if(category.equals(row.getKategoria())) {
+                expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
+            }
         }
-
+        }
        return new ExpensesOutput(expenses);
     }
+
 }
