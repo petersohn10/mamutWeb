13104e2538af133187915542da1b6e0df1397c23
Piotr Zorychta <petersohn100@gmail.com>
2022-04-25 21:21:58 +0200

    add excluded category

--- mamutweb/src/main/java/com/lacio/mamutweb/ExpensesController.java
+++ mamutweb/src/main/java/com/lacio/mamutweb/ExpensesController.java
@@ -1,6 +1,7 @@
 package com.lacio.mamutweb;
 
 import com.lacio.mamutweb.entities.SettlementRow;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,23 +18,31 @@ public class ExpensesController {
     }
 
     @GetMapping("/expenses")
-    public ExpensesOutput expensesOutput(@RequestParam(required = false) String category){
-
-
+    public ResponseEntity<ExpensesOutput> expensesOutput(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String excludedCategory
+    ) {
+        if (category != null && excludedCategory != null) {
+            return ResponseEntity.badRequest().build();
+        }
         ArrayList<Expense> expenses = new ArrayList<>();
         List<SettlementRow> rows = repository.findAll();
-        if (category==null) {
+        if (category == null && excludedCategory == null) {
             for (SettlementRow row : rows) {
                 expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
             }
         } else {
-        for (SettlementRow row : rows) {
-            if(category.equals(row.getKategoria())) {
-                expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
+            for (SettlementRow row : rows) {
+                if (category != null && category.equals(row.getKategoria())) {
+                    expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
+                }
+                if (excludedCategory != null && !excludedCategory.equals(row.getKategoria())) {
+                    expenses.add(new Expense(row.getId(), row.getKategoria(), row.getKoszt(), Character.toString(row.getKto())));
+                }
+
             }
         }
-        }
-       return new ExpensesOutput(expenses);
+        return ResponseEntity.ok().body(new ExpensesOutput(expenses));
     }
 
 }
